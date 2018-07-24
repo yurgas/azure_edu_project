18581583ae2b25a7a8348a96fdc7ea0f8f4acf56
yurgas <yurarsit@gmail.com>
2018-07-24 19:04:31 +0300

    Initial commit of provisioning script

---
+++ provision/run_provision.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+CONFIG_FILE=$1
+DB_NAME=$2
+DB_USER=$3
+DB_PASSWORD=$4
+DB_HOST=$5
+
+#Install required packages and WordPress
+
+apt-get update
+apt-get install -y apache2
+apt-get install -y php libapache2-mod-php php-mcrypt php-mysql php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
+cd /tmp
+curl -O https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+touch /tmp/wordpress/.htaccess
+chmod 660 /tmp/wordpress/.htaccess
+rm -rf /var/www/html/*
+cp -a /tmp/wordpress/. /var/www/html
+chown -R root:www-data /var/www/html
+chmod g+w /var/www/html/wp-content
+chmod -R g+w /var/www/html/wp-content/themes
+chmod -R g+w /var/www/html/wp-content/plugins
+systemctl restart apache2
+
+#Generate wordpress configuration file
+
+exec 1>$CONFIG_FILE
+
+echo "<?php"
+echo
+echo "define('DB_NAME', '$DB_NAME');"
+echo "define('DB_USER', '$DB_USER');"
+echo "define('DB_PASSWORD', '$DB_PASSWORD');"
+echo "define('DB_HOST', '$DB_HOST');"
+echo "define('DB_CHARSET', 'utf8');"
+echo "define('DB_COLLATE', '');"
+echo
+
+curl -s https://api.wordpress.org/secret-key/1.1/salt/
+
+echo
+echo "\$table_prefix  = 'wp_';"
+echo
+echo "define('WP_DEBUG', false);"
+echo
+echo "if ( !defined('ABSPATH') )"
+echo "	define('ABSPATH', dirname(__FILE__) . '/');"
+echo
+echo "require_once(ABSPATH . 'wp-settings.php');"
